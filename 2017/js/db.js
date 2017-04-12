032fbaeb7a58be32d6ccd900267cc15865395ff3
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2017-04-12 15:01:48 -0300

    atualiza banco para teste

--- 2017/js/db.js
+++ 2017/js/db.js
@@ -1,9 +1,19 @@
 var caccomp_bd = {}
 
 caccomp_bd["pessoas"] = {
-    "0":{
+    "0":{                        
         "nome":"Paulo Roberto Cruz França",
-        "email":"paulo.cruz9@gmail.com",
-        "github":"paulorobertocruz"
+        "email":"paulocruz9@gmail.com",
+        "curso":"Ciência da Computação",
+        "github":"paulorobertocruz",        
+        "facebook":"",
+        "image-perfil":"http://secompuft.github.io/bd/2017/img/pessoa0.png",
+        "minicursos":["0"],
+        "palestras":["0"]
+    },
+    "1":{
+        "nome":"Adelmo Junior Teixeira",
+        "email":"adelmo@gmail.com",
+        "github":"noblerealm"
     }
 }

377a67fc4130e08cd875d1dbe69ba48bf6e0b4a6
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2017-04-12 15:30:02 -0300

    adiciona template para cronograma

--- 2017/js/db.js
+++ 2017/js/db.js
@@ -1,7 +1,23 @@
 var caccomp_bd = {}
 
+caccomp_bd["dias"] = {
+    "12/04/2017":{
+        "palestras":[],
+        "minicursos":[]
+    },
+    "13/04/2017":{
+        "palestras":[],
+        "minicursos":[]
+    },
+    "14/04/2017":{
+        "palestras":[],
+        "minicursos":[]
+    }
+
+}
+
 caccomp_bd["pessoas"] = {
-    "0":{                        
+    "0":{                      
         "nome":"Paulo Roberto Cruz França",
         "email":"paulocruz9@gmail.com",
         "curso":"Ciência da Computação",

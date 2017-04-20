acbf4cbb13fb36ef374ae35516fdc6ba8fdb46d2
Larissa Cristina <leurissy@gmail.com>
2017-04-20 20:47:50 -0300

    Adicinado Cronograma e palestras

--- 2017/js/db.js
+++ 2017/js/db.js
@@ -8,13 +8,262 @@ caccomp_bd["dias"] = {
     "12/04/2017":{
         [
             {
-                "horario_inicio":"",
-                "horario_fim":"",
-                "titulo":"",
+                "horario_inicio":"08:00:00",
+                "horario_fim":"08:30:00",
+                "titulo":"Credenciamento",
                 "pessoas":["0"],
-                "periodo":"",
+                "periodo":"Manhã",
                 "palestras":false,
                 "minicursos":false
+            },
+
+            {
+                "horario_inicio":"08:30:00",
+                "horario_fim":"09:00:00",
+                "titulo":"Abertura",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"09:00:00",
+                "horario_fim":"10:00:00",
+                "titulo":"Palestra de Abertura",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":true,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"10:00:00",
+                "horario_fim":"10:15:00",
+                "titulo":"Café Regional",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"10:15:00",
+                "horario_fim":"11:00:00",
+                "titulo":"Sessão Tecnica I",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"11:00:00",
+                "horario_fim":"12:00:00",
+                "titulo":"Palestra II",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":true,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"12:00:00",
+                "horario_fim":"14:00:00",
+                "titulo":"Intervalo pro Almoço",
+                "pessoas":false,
+                "periodo":" ",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"14:00:00",
+                "horario_fim":"18:00:00",
+                "titulo":"Minicursos",
+                "pessoas":false,
+                "periodo":"Tarde",
+                "palestras":false,
+                "minicursos":true
+            },
+
+            {
+                "horario_inicio":"18:00:00",
+                "horario_fim":"19:00:00",
+                "titulo":"Intervalo pro Jantar",
+                "pessoas":false,
+                "periodo":" ",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"19:00:00",
+                "horario_fim":"22:30:00",
+                "titulo":"GameNight",
+                "pessoas":["0"],
+                "periodo":"Noite",
+                "palestras":false,
+                "minicursos":true
+            }
+        ]
+    }
+
+    "13/04/2017":{
+        [
+
+            {
+                "horario_inicio":"08:30:00",
+                "horario_fim":"09:00:00",
+                "titulo":"Palestra",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":true,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"09:00:00",
+                "horario_fim":"10:00:00",
+                "titulo":"Palestra IV",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":true,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"10:00:00",
+                "horario_fim":"10:15:00",
+                "titulo":"Café Regional",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"10:15:00",
+                "horario_fim":"12:00:00",
+                "titulo":"Sessão Tecnica II",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"12:00:00",
+                "horario_fim":"14:00:00",
+                "titulo":"Intervalo pro Almoço",
+                "pessoas":false,
+                "periodo":" ",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"14:00:00",
+                "horario_fim":"18:00:00",
+                "titulo":"Minicursos",
+                "pessoas":false,
+                "periodo":"Tarde",
+                "palestras":false,
+                "minicursos":true
+            },
+
+            {
+                "horario_inicio":"18:00:00",
+                "horario_fim":"19:00:00",
+                "titulo":"Intervalo pro Jantar",
+                "pessoas":false,
+                "periodo":" ",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"19:00:00",
+                "horario_fim":"22:30:00",
+                "titulo":"GameNight",
+                "pessoas":["0"],
+                "periodo":"Noite",
+                "palestras":false,
+                "minicursos":true
+            }
+        ]
+    }
+
+    "14/04/2017":{
+        [
+            {
+                "horario_inicio":"08:30:00",
+                "horario_fim":"10:00:00",
+                "titulo":"Palestra V",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":true,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"10:00:00",
+                "horario_fim":"10:15:00",
+                "titulo":"Café Regional",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"10:15:00",
+                "horario_fim":"12:00:00",
+                "titulo":"Sessão Tecnica III",
+                "pessoas":["0"],
+                "periodo":"Manhã",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"12:00:00",
+                "horario_fim":"14:00:00",
+                "titulo":"Intervalo pro Almoço",
+                "pessoas":false,
+                "periodo":" ",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"14:00:00",
+                "horario_fim":"18:00:00",
+                "titulo":"Minicursos",
+                "pessoas":false,
+                "periodo":"Tarde",
+                "palestras":false,
+                "minicursos":true
+            },
+
+            {
+                "horario_inicio":"18:00:00",
+                "horario_fim":"19:00:00",
+                "titulo":"Intervalo pro Jantar",
+                "pessoas":false,
+                "periodo":" ",
+                "palestras":false,
+                "minicursos":false
+            },
+
+            {
+                "horario_inicio":"19:00:00",
+                "horario_fim":"22:30:00",
+                "titulo":"GameNight",
+                "pessoas":["0"],
+                "periodo":"Noite",
+                "palestras":false,
+                "minicursos":true
             }
         ]
     }
@@ -42,4 +291,21 @@ caccomp_bd["pessoas"] = {
         "minicursos":["0"],
         "palestras":["0"]
     }
+    "2":{
+        "nome":"Maxwell Diógenes Bandeira de Melo",
+        "email":false,
+        "curso":"Engenharia Elétrica",
+        "github":false,        
+        "facebook":false,
+        "lattes":false,
+        "imagem-perfil":"http://secompuft.github.io/bd/2017/img/pessoas/fulano.jpg",
+        "minicursos":["0"],
+        "palestras":["0"]
+    }
+caccomp_bd["palestras"] = {
+    "0":{
+        "palestrante":"2",
+        "nome":false
+    }
+}
 }
